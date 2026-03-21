d013ba22801ec2dc91b221a934c32c9eb0e014c9
3barney <brnbsjm@gmail.com>
2026-03-21 21:23:06 +0300

    setup logging lib base

---
+++ src/test/java/com/github/barney/canonicallog/CanonicalLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.barney.canonicallog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanonicalLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
