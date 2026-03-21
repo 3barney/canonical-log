ff630636f2468ebdf42645ad881c470af8fa8feb
3barney <brnbsjm@gmail.com>
2026-03-22 00:35:33 +0300

    configuration setup for jpa entity listeners, http client interceptor and main log filter linkage to the canonical log context and events

--- src/main/java/com/github/barney/canonicallog/app/config/CanonicalLogAppAutoConfiguration.java
+++ src/main/java/com/github/barney/canonicallog/app/config/CanonicalLogAppAutoConfiguration.java
@@ -7,12 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-@ComponentScan(basePackages = "com.github.barney.canonicallog")
+@ComponentScan(basePackages = "com.github.barney.canonicallog.app")
 public class CanonicalLogAppAutoConfiguration {
 
     @Bean
-    public RestClient canonicalRestClient(RestClient.Builder builder) {
-        return builder
+    public RestClient canonicalRestClient() {
+        return RestClient.builder()
                 .requestInterceptor(new ApplicationClientHttpInterceptor())
                 .build();
     }
